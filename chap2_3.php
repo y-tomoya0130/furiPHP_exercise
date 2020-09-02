3c91d75f09cb533f6848b598d480e1f1fd6c9f2c
y-tomoya0130 <69039532+y-tomoya0130@users.noreply.github.com>
2020-09-02 23:21:19 +0900

    Update chap2_3.php

--- chap2_3.php
+++ chap2_3.php
@@ -3,10 +3,10 @@ var_dump("数学の点数を入力してください");
 $score_1 = trim(fgets(STDIN));
 var_dump("国語の点数を入力してください");
 $score_2 = trim(fgets(STDIN));
-if( $score_1 + $score_2 >= 80 ){
+if( $score_1 && $score_2 >= 80 ){
     var_dump("合格です");
-}elseif( $score_1 + $score_2 < 50 ){
-    var_dump("不合格です");
-}else{
+}elseif( $score_1  >= 50 or $score_2 >= 50 ){
     var_dump("追試です");
+}elseif( $score_1 < 50 && $score_2 < 50 ) {
+    var_dump("不合格です");
 }
