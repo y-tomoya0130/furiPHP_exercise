62d5db6c6a06c665bc8a275fdedf9723008a46be
y-tomoya0130 <69039532+y-tomoya0130@users.noreply.github.com>
2020-09-11 23:00:04 +0900

    Update chap3_3.php
    ご指摘ありがとうございます！修正いたしました！

--- chap3_3.php
+++ chap3_3.php
@@ -1,12 +1,12 @@
 <?php
-for($bai3 = 1; $bai3 <= 100; $bai3++){
+for ($bai3 = 1; $bai3 <= 100; $bai3++){
     if ($bai3 % 3 == 0 && $bai3 % 5 == 0) {
         print_r(" FizzBuzz ");
-    }elseif($bai3 % 5 == 0){
-        print_r( " Buzz ");
-    }elseif($bai3 % 3 == 0){
+    } elseif ($bai3 % 5 == 0){
+        print_r(" Buzz ");
+    } elseif ($bai3 % 3 == 0){
         print_r(" Fuzz ");
-    }else{
-        print_r( $bai3 . " ");
+    } else {
+        print_r($bai3 . " ");
     }
 }
